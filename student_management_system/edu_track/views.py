1f7f3bdc2c1a9ecd81df56e763b430bc9d12d557
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-09 14:53:32 +0545

    Added CRUD operations logic for Courses

--- student_management_system/edu_track/views.py
+++ student_management_system/edu_track/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Course, Module, Student, Result, Attendance
 
-# Create your views here.
+#Fetch all courses from Database and Send it to template
+def list_courses(request):
+    courses = Course.objects.all()
+    return render(request, "course_list.html", {"courses": courses})
+
+#Add new courses
+def course_add(request):
+    if request.method == "POST":
+        name = request.POST.get("course_name")
+        duration = request.POST.get("course_duration")
+
+        Course.objects.create(
+            course_name=name,
+            course_duration=duration
+        )
+        return redirect("course_list")
+
+    return render(request, "course_add.html")
+
+#Update courses
+def course_update(request, id):
+    course = Course.objects.get(id=id)
+
+    if request.method == "POST":
+        course.course_name = request.POST.get("course_name")
+        course.course_duration = request.POST.get("course_duration")
+        course.save()
+        return redirect("course_list")
+
+    return render(request, "course_update.html", {"course": course})
+
+#Delete courses
+def course_delete(request, id):
+    course = Course.objects.get(id=id)
+    course.delete()
+    return redirect("course_list")
