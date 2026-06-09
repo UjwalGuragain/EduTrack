b567d68d8f50feaddf365adea22ead156229d9a2
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-09 11:38:32 +0545

    Added initial models for EduTrack

--- StudentManagementSystem/EduTrack/models.py
+++ StudentManagementSystem/EduTrack/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_name  = models.CharField(max_length=100)
+    DURATION_CHOICES = [
+        (1, "1 Year"),
+        (2, "2 Years"),
+        (3, "3 Years"),
+        (4, "4 Years"),
+        (5, "5 years")
+    ]
+    course_duration = models.IntegerField(choices = DURATION_CHOICES)
+
+    def __str__(self):
+        return self.course_name
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=20, unique=True)
+    full_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=10)
+    email = models.EmailField()
+    guardian_name = models.CharField(max_length=100)
+    enrolled_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment_date = models.DateField()
+
+    def __str__(self):
+        return self.full_name
+    
