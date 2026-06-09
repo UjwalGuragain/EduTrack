0495311fd5ba7ecad0910af05d9de4574e52be69
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-09 16:01:32 +0545

    Added URL paths

--- student_management_system/edu_track/urls.py
+++ student_management_system/edu_track/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-
+path("course/", views.list_courses, name = "course_list"),
+path("course/add/", views.course_add, name = "course_add"),
+path("course/update/<int:id>/", views.course_update, name = "course_update"),
+path("course/delete/<int:id>", views.course_delete, name = "course_delete"),
 ]
