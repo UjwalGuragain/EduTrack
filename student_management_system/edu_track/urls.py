04461b07ff026022514b06197161c9dd296235ef
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-11 15:44:50 +0545

    feat: Added URL Paths

--- student_management_system/edu_track/urls.py
+++ student_management_system/edu_track/urls.py
@@ -7,4 +7,9 @@ path("course/", views.list_courses, name = "course_list"),
 path("course/add/", views.course_add, name = "course_add"),
 path("course/update/<int:id>/", views.course_update, name = "course_update"),
 path("course/delete/<int:id>", views.course_delete, name = "course_delete"),
+
+path("student/", views.list_students, name = "student_list"),
+path("student/add/", views.student_add, name = "student_add"),
+path("student/update/<int:id>/", views.student_update, name = "student_update"),
+path("student/delete/<int:id>", views.student_delete, name = "student_delete"),
 ]
