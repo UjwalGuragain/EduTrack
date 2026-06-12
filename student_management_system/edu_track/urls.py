58c4fc947ddc6048e0651d137525d244d225c0f4
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-12 15:38:33 +0545

    feat: Add URL Patterns

--- student_management_system/edu_track/urls.py
+++ student_management_system/edu_track/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+path("dashboard/", views.dashboard, name = "dashboard"),
+
 path("course/", views.list_courses, name = "course_list"),
 path("course/add/", views.course_add, name = "course_add"),
 path("course/update/<int:id>/", views.course_update, name = "course_update"),
