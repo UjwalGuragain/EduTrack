ca920a55f46e570d70b2f909123e46ca0b94e492
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-16 09:53:05 +0545

    feat: registered new model

--- student_management_system/edu_track/admin.py
+++ student_management_system/edu_track/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Course, Module, Student, Result, Attendance
+from .models import Course, Module, Student, Result, Attendance, Instructor
 
 admin.site.register(Course)
 admin.site.register(Module)
 admin.site.register(Student)
 admin.site.register(Result)
 admin.site.register(Attendance)
+admin.site.register(Instructor)
