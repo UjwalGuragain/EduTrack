629806275071fb5a48116ecc699f7ce783013340
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-19 11:18:44 +0545

    feat: add configurations in settings to send email

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -118,3 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+
+PASSWORD_RESET_TIMEOUT = 300
