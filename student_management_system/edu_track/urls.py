7139699fccbba76d1d2ac9179b79861ac34647a1
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-19 12:08:12 +0545

    feat: configure URL patterns

--- student_management_system/edu_track/urls.py
+++ student_management_system/edu_track/urls.py
@@ -12,6 +12,11 @@ path("password-reset/", auth_views.PasswordResetView.as_view(
     template_name="edu_track/registration/password_reset.html",
     html_email_template_name="edu_track/registration/password_reset_email.html"),
     extra_email_context = {"domain" : "edutrack.com", "protocol" : "https"}, name = "password_reset"),
+path("password-reset/done/",auth_views.PasswordResetDoneView.as_view(template_name="edu_track/registration/password_reset_done.html"), name="password_reset_done"),
+path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="edu_track/registration/password_reset_confirm.html"), name="password_reset_confirm"),
+path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="edu_track/registration/password_reset_complete.html"),name="password_reset_complete"),
+path("password-change/", auth_views.PasswordChangeView.as_view(template_name="edu_track/registration/password_change.html"), name = "password_change"),
+path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(template_name="edu_track/registration/password_change_done.html"), name = "password_change_done"),
 
 path("instructor-dashboard/", views.instructor_dashboard, name = "instructor_dashboard"),
 path("student-dashboard/", views.student_dashboard, name = "student_dashboard"),
