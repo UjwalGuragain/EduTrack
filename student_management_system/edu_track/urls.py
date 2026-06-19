aa65b242d1364ea47b1488aa7511ef6b47e52c66
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-06-19 11:20:13 +0545

    chore: update url paterns for password reset

--- student_management_system/edu_track/urls.py
+++ student_management_system/edu_track/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,8 +8,10 @@ path("", views.user_login, name = "login"),
 path("login/", views.user_login, name = "login"),
 path("register/", views.user_register, name = "register"),
 path("logout/", views.user_logout, name = "logout"),
-path("password-reset/", views.password_reset, name = "password_reset"),
-
+path("password-reset/", auth_views.PasswordResetView.as_view(
+    template_name="edu_track/registration/password_reset.html",
+    html_email_template_name="edu_track/registration/password_reset_email.html"),
+    extra_email_context = {"domain" : "edutrack.com", "protocol" : "https"}, name = "password_reset"),
 
 path("instructor-dashboard/", views.instructor_dashboard, name = "instructor_dashboard"),
 path("student-dashboard/", views.student_dashboard, name = "student_dashboard"),
@@ -17,6 +20,7 @@ path("course/", views.list_courses, name = "course_list"),
 path("course/add/", views.course_add, name = "course_add"),
 path("course/update/<int:id>/", views.course_update, name = "course_update"),
 path("course/delete/<int:id>/", views.course_delete, name = "course_delete"),
+path("my-course/", views.my_course, name = "my_course"),
 
 path("student/", views.list_students, name = "student_list"),
 path("student/add/", views.student_add, name = "student_add"),
@@ -28,14 +32,17 @@ path("module/", views.list_modules, name = "module_list"),
 path("module/add/", views.module_add, name = "module_add"),
 path("module/update/<int:id>/", views.module_update, name = "module_update"),
 path("module/delete/<int:id>/", views.module_delete, name = "module_delete"),
+path("my-module/", views.my_module, name = "my_module"),
 
 path("result/", views.list_result, name = "result_list"),
 path("result/add/", views.result_add, name = "result_add"),
 path("result/update/<int:id>/", views.result_update, name = "result_update"),
 path("result/delete/<int:id>/", views.result_delete, name = "result_delete"),
+path("my-result/", views.my_result, name = "my_result"),
 
 path("attendance/", views.list_attendance, name = "attendance_list"),
 path("attendance/add/", views.attendance_add, name = "attendance_add"),
 path("attendance/update/<int:id>/", views.attendance_update, name = "attendance_update"),
-path("attendance/delete/<int:id>/", views.attendance_delete, name = "attendance_delete")
+path("attendance/delete/<int:id>/", views.attendance_delete, name = "attendance_delete"),
+path("my-attendance/", views.my_attendance, name = "my_attendance"),
 ]
