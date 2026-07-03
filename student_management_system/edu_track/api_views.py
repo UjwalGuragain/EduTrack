d0a1458743db647436f153ef95049630a4b99983
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-07-03 11:47:00 +0545

    feat: add student_api views

---
+++ student_management_system/edu_track/api_views.py
@@ -0,0 +1,26 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import *
+from .serializers import *
+from rest_framework import status
+
+@api_view(["GET", "POST"])
+def student_api(request):
+    
+    if request.method == "GET":
+
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many = True)
+       
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    elif request.method == "POST":
+
+        serializer = StudentSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
