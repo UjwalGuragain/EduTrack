f6bbf4b32ee4108c161afc72371ecda04e91cb09
Ujwal Guragain <ujwalguragain436@gmail.com>
2026-07-04 20:45:00 +0545

    feat: add instructor_api views

--- student_management_system/edu_track/api_views.py
+++ student_management_system/edu_track/api_views.py
@@ -24,3 +24,24 @@ def student_api(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "POST"])
+def instructor_api(request):
+
+    if request.method == "GET":
+
+        instructors = Instructor.objects.all()
+        serializer = InstructorSerializer(instructors, many = True)
+
+        return Response(serializer.data, staus = status.HTTP_200_OK)
+
+    elif request.method == "POST":
+
+        serializer = InstructorSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
